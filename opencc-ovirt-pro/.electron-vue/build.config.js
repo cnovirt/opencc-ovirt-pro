b3a1b21eb1c735b121288188715ee522d6de2ece
wang_song <zero_ws@163.com>
2021-04-28 15:09:11 +0800

    更新logo图标,标准化大小256x256

--- opencc-ovirt-pro/.electron-vue/build.config.js
+++ opencc-ovirt-pro/.electron-vue/build.config.js
@@ -8,7 +8,7 @@ module.exports = {
   arch: 'x64',
   asar: true,
   dir: path.join(__dirname, '../'),
-  icon: path.join(__dirname, '../build/icons/openCC_logo'),
+  icon: path.join(__dirname, '../build/icons/opencc_logo'),
   ignore: /(^\/(src|test|\.[a-z]+|README|yarn|static|dist\/web))|\.gitkeep/,
   out: path.join(__dirname, '../build'),
   overwrite: true,
