3ced4f93a53671d6b1fd751e84a9f6e68e82f2d0
wang_song <zero_ws@163.com>
2021-07-06 13:57:50 +0800

    限制一个客户端运行

--- opencc-ovirt-pro/src/main/index.js
+++ opencc-ovirt-pro/src/main/index.js
@@ -57,3 +57,37 @@ function createWindow() {
 }
 
 app.on('ready', createWindow)
+
+// 限制只可以打开一个应用,2.x的文档
+const isSecondInstance = app.makeSingleInstance(
+  (commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+      mainWindow.show()
+    }
+  }
+)
+
+if (isSecondInstance) {
+  app.quit()
+}
+
+// 限制只可以打开一个应用, 4.x的文档
+// const gotTheLock = app.requestSingleInstanceLock()
+// if (!gotTheLock) {
+//   app.quit()
+// } else {
+//   app.on('second-instance', (event, commandLine, workingDirectory) => {
+//     // 当运行第二个实例时,将会聚焦到mainWindow这个窗口
+//     if (mainWindow) {
+//       if (mainWindow.isMinimized()) mainWindow.restore()
+//       mainWindow.focus()
+//       mainWindow.show()
+//     }
+//   })
+//   // 创建 myWindow, 加载应用的其余部分, etc...
+//   // app.on('ready', () => {
+//   // })
+// }
